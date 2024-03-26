af48a0adf5c0221d84465721cd94d6ef1b92927f
tommylam0915 <tommylam0915@gmail.com>
2024-03-26 17:04:59 +0800

    update

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld{
-  public static void main(String[]args){
-    //Start of the program 
-    //Tool 1 : System.out.println()
-    System.out.println("Hello world, I'm Tommy");
-    System.out.println("Help");    
-  }
-}
